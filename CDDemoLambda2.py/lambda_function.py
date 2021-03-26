128c379253218ca9e11005444c822aa3315c5143
Gladwin <gladwin.neo.2018@sis.smu.edu.sg>
2021-03-27 00:03:11 +0800

    test

--- CDDemoLambda2.py/lambda_function.py
+++
@@ -1,8 +0,0 @@
-import boto3
-import json
-
-ec2 = boto3.client('ec2')
-def lambda_handler(event, context):
-    print("hello world!")
-    response = ec2.describe_availability_zones()
-    return {"statusCode": 200, "body": json.dumps(response)}
