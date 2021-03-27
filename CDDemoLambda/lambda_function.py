2c9765bb7be03606657ea6d6a6329b8f5a68e90b
Gladwin <gladwin.neo.2018@sis.smu.edu.sg>
2021-03-27 12:58:11 +0800

    deployment test 2

--- CDDemoLambda/lambda_function.py
+++ CDDemoLambda/lambda_function.py
@@ -3,6 +3,6 @@ import json
 
 ec2 = boto3.client('ec2')
 def lambda_handler(event, context):
-    print("lambda function 1")
+    print("lambda function 1 after deploy")
     response = ec2.describe_availability_zones()
     return {"statusCode": 200, "body": json.dumps(response)}
